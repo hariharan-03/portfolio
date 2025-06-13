2dffabe781cc0b0d5357c49b22b84da5a54443b1
hariharan-03 <60616827+hariharan-03@users.noreply.github.com>
2025-06-13 21:49:29 +0530

    Update home.js

--- components/home.js
+++ components/home.js
@@ -4,7 +4,7 @@ export default function home() {
   return (
           <Card className="px-5 py-3 maincard dark-bg text-white">
           <h2 className='mb-4'>ABOUT ME</h2>
-          <p className='ms-3 mb-3'>A passionate web developer with 4+ years of experience in designing and developing user interfaces, testing, debugging, and training staff within eCommerce technologies. Proven ability in optimizing web functionalities that improve data retrieval and workflow efficiencies.</p>
+          <p className='ms-3 mb-3'>A passionate web developer with 5+ years of experience in designing and developing user interfaces, testing, debugging, and training staff within eCommerce technologies. Proven ability in optimizing web functionalities that improve data retrieval and workflow efficiencies.</p>
           <h4>Web stack</h4>
           <Row className='mb-3'>
             <Col className='mb-3 ms-2'>
