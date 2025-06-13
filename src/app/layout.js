9aad71275f9080381c063daa5e9f7be1416e5a0c
hariharan-03 <60616827+hariharan-03@users.noreply.github.com>
2025-06-13 21:55:13 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,7 +29,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="h-100">{children}</body>
     </html>
   )
 }
