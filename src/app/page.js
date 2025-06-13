0468a107ae3d6321e7c754c0bb53c6a80bf9f54d
hariharan-03 <60616827+hariharan-03@users.noreply.github.com>
2025-06-13 21:56:43 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Contact from "../../components/contact"
 export default function Index() {
   return (
     <>
-   <Container className="pt-4 pb-3">
+   <Container className="pt-5 pb-3">
       <Row>
         <Col md={4} className="my-auto mt-5 pt-5">
           <Card className="dark-bg text-white withborder">
